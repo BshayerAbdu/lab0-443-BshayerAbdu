04d0c02e0f831af05f6265bb7bb982f979e48483
BshayerAbdu <120296818+BshayerAbdu@users.noreply.github.com>
2023-03-21 17:15:28 +0000

    I am the student who told you about github.  It keeps saying there is an error, even though the code is correct when I try on NetBeans

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -1,4 +1,5 @@
 package edu.uqu.cs;
+import java.util.Scanner;
 /*
  * Lab 0 
  * CS 1312
@@ -24,6 +25,7 @@ public class App{
 *        TweetleBeetlePoodle 13 Tweetle 15 TweetleBeetle 17 TweetlePoodle 19 TweetleBeetle 
 */
 
+
 /**
 * Write a method named "phoneKeypad" that takes a string parameter (str)
 * then it replaces each letter in (str) by a number that represnts it 
@@ -42,7 +44,56 @@ public class App{
 * Output: 2255464228626
 *
 */
- 
+//////**** first method ****/////
+ public static void twisters(){
+     for(int count =1;count<=20;count++){
+        if(count%2==0){
+          System.out.print("Tweetle");
+         if(count%4==0)
+           System.out.print("Beetle");
+          if(count%6==0)  
+            System.out.print("Poodle");}
+     else
+            System.out.print(" "+count+" ");}
+     System.out.println();
+   }
+   /////**** second method ****/////
+           public static void phoneKeypad (String str){  
+        str= str.toLowerCase();   
+      for(int count =0; count<str.length();count++){
+          if(str.charAt(count)==' ')
+              continue;        
+      switch(str.charAt(count){
+          case 'a','b','c':
+              System.out.print("2");
+              break;
+          case 'e','d','f':
+              System.out.print("3");
+              break;
+          case 'g','h','i':
+              System.out.print("4");
+              break;
+          case 'j','k','l':
+              System.out.print("5");
+              break;   
+          case 'm','n','o':
+              System.out.print("6");
+              break;   
+          case 'p','q','r','s':
+              System.out.print("7");
+              break;   
+          case 't','u','v':
+              System.out.print("8");
+              break; 
+          default:
+              System.out.print("9");
+              break;
+      }}
+  }
+    /////
+    //I am the student who told you about github.
+    //It keeps saying there is an error, even though the code is correct when I try on NetBeans
+    /////
     public static void main(String [] args) {
         
 
@@ -51,7 +102,13 @@ public class App{
        //call method twisters()
        //prompt user to enter a string 
        //call method phoneKeypad(string)
-
+       Scanner in=new Scanner(System.in);
+        twisters();
+        
+        System.out.println("Enter a sentence");
+        String str=in.nextLine();
+         phoneKeypad(str);
+        
 
     }
 
